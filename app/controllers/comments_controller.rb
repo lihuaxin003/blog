b8c163e2f9175c9a925384c8f38e75bf6960d0f1
lhx <342542@qq.com>
2012-05-03 20:08:00 +0800

    my first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController 
+
+  http_basic_authenticate_with :name => "dhh", :password => "secret", :only => :destroy 
+  
+  def create   
+    @post = Post.find(params[:post_id])   
+    @comment = @post.comments.create(params[:comment])   
+    redirect_to post_path(@post)   
+  end  
+  
+  def destory
+    @post = Post.find(params[:post_id])   
+    @comment = @post.comments.find(params[:id])
+    @comment.destory
+    redirect_to post_path(@post) 
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c41dafbc83f7d97fe3d335687f88b3b1f43c6d817cf3ca68fddd1427e5499528ab815b7edf35b17678b39fc1c21d37067077d7705184fee2da893a6f73d236d6'
